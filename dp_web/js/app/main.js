f8ab366bf2f0cb41e19df9f350d81d0cfb4c4d91
cheer021 <cheer021@gmail.com>
2015-10-11 22:29:58 -0400

    http call to get json and display; added filter

---
+++ dp_web/js/app/main.js
@@ -0,0 +1,8 @@
+define(["views/ListView","jquery","angular"], 
+    function( ListView,jquery, angular) {
+    var obj = function(){
+        var listview = new ListView();
+        return listview;
+    };,
+    return obj;
+});
